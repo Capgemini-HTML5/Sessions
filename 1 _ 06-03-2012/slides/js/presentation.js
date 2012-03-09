20f7b4e4addff51263cb8ef52946134679f1c269
Willem Mulder <willemmulder@gmail.com>
2012-03-09 20:26:42 +0100

    Prepare for dedicated .js release. Now works on Firefox, Chrome and Opera.

--- 1 _ 06-03-2012/slides/js/presentation.js
+++ 1 _ 06-03-2012/slides/js/presentation.js
@@ -5,7 +5,7 @@ function Presentation(canvas, elements) {
 		alert('geen geldige canvas opgegeven');
 		return;
 	}
-	canvasZoomFactor = 1;
+	var canvasZoomFactor = 1;
 	var elements = elements; // Array of elements to show, in order
 	/*
 		elements = [];
@@ -31,11 +31,14 @@ function Presentation(canvas, elements) {
 		}
 	*/
 	var currentIndex = -1;
+	var options = { showOriginalMargins : false, showCustomMargin : false, customMarginWidth : 20 };
 	
 	function show(elm) {
 		var e = $(elm.element);
+		resetTransformations();
 		// Calculate base-position (i.e. with zoomFactor 1) for Webkit
-		if ($.browser.webkit) {
+		// TODO: get left while taking margin into account
+		if ($.browser.webkit || $.browser.opera) {
 			var baseLeft = ((e.offset().left  - canvas.offset().left) / canvasZoomFactor);
 			var baseTop = ((e.offset().top  - canvas.offset().top) / canvasZoomFactor);
 		} else {
@@ -43,45 +46,82 @@ function Presentation(canvas, elements) {
 			var baseTop = e.offset().top  - canvas.offset().top;
 		}					
 		// zoom so that the element fits on screen
-		var canvasContainerWidth = canvas.outerWidth();
-		var canvasContainerHeight = canvas.outerHeight();
-		var proportionalWidth = e.outerWidth() / canvasContainerWidth; // e.g. 200/1000 = 0.2
-		var proportionalHeight = e.outerHeight() / canvasContainerHeight;
+		var canvasContainerWidth = canvas.outerWidth(options.showOriginalMargins);
+		var canvasContainerHeight = canvas.outerHeight(options.showOriginalMargins);
+		var proportionalWidth = e.outerWidth(options.showOriginalMargins) / canvasContainerWidth; // e.g. 200/1000 = 0.2
+		var proportionalHeight = e.outerHeight(options.showOriginalMargins) / canvasContainerHeight;
 		var scaleFactor = Math.max(proportionalWidth, proportionalHeight);
 		canvasZoomFactor = (1 / scaleFactor); // e.g. zoom to (1 / (0.2)) = 5
-		zoom(canvas, canvasZoomFactor);
 		// Move element. Always move the element to the center of the screen
 		var newLeft = (baseLeft * canvasZoomFactor * -1);
 		var newTop = (baseTop * canvasZoomFactor * -1);
 		if (proportionalWidth > proportionalHeight) {
 			// Element will take full Width, leaving space at top and bottom
 			// Calculate rest-space
-			var openSpace = canvasContainerHeight - (e.outerHeight()*canvasZoomFactor);
-			newTop += (openSpace / 2 * canvasZoomFactor);
+			var openSpace = canvasContainerHeight - (e.outerHeight(options.showOriginalMargins)*canvasZoomFactor);
+			newTop += (openSpace / 2);
 		} else {
 			// Element will take full Height, leaving space left and right
-			var openSpace = canvasContainerWidth - (e.outerWidth()*canvasZoomFactor);
-			newLeft += (openSpace / 2 * canvasZoomFactor);
+			var openSpace = canvasContainerWidth - (e.outerWidth(options.showOriginalMargins)*canvasZoomFactor);
+			newLeft += (openSpace / 2);
 		}
 		// If canvas is smaller than its container, then center the canvas in its parent
-		/*
-		if ((canvas.outerHeight() * canvasZoomFactor) < canvas.parent().outerHeight()) {
-			newTop = (canvas.parent().outerHeight() - (canvas.outerHeight() * canvasZoomFactor)) / 2;
-		}*/
-		if ((canvas.outerWidth() * canvasZoomFactor)  < canvas.parent().outerWidth()) {
-			newLeft = (canvas.parent().outerWidth() - (canvas.outerWidth() * canvasZoomFactor)) / 2;
+		if ((outerScrollHeight(canvas, options.showOriginalMargins) * canvasZoomFactor) < canvas.parent().outerHeight(options.showOriginalMargins)) {
+			newTop = (canvas.parent().outerHeight(options.showOriginalMargins) - (outerScrollHeight(canvas, options.showOriginalMargins) * canvasZoomFactor)) / 2;
 		}
-		move(canvas, newLeft, newTop);
+		if ((outerScrollWidth(canvas, options.showOriginalMargins) * canvasZoomFactor)  < canvas.parent().outerWidth(options.showOriginalMargins)) {
+			newLeft = (canvas.parent().outerWidth(options.showOriginalMargins) - (outerScrollWidth(canvas, options.showOriginalMargins) * canvasZoomFactor)) / 2;
+		}
+		move(newLeft, newTop);
+		zoom(canvasZoomFactor);
 	}
 	
-	function zoom(elm, zoomLevel) {
-		$(elm).get(0).style.MozTransform = 'scale('+zoomLevel+')';
-		$(elm).get(0).style.WebkitTransform = 'scale('+zoomLevel+')';
+	function resetTransformations() {
+		$(canvas).get(0).style.MozTransform = "";
+		$(canvas).get(0).style.WebkitTransform = "";
+		$(canvas).get(0).style.OTransform = "";
 	}
 	
-	function move(elm, left, top) {
-		$(elm).css("left", left);
-		$(elm).css("top", top);
+	function zoom(zoomLevel) {
+		$(canvas).get(0).style.MozTransform += 'scale('+zoomLevel+')';
+		$(canvas).get(0).style.WebkitTransform += 'scale('+zoomLevel+')';
+		$(canvas).get(0).style.OTransform += 'scale('+zoomLevel+')';
+	}
+	
+	function move(left, top) {
+		$(canvas).get(0).style.MozTransform += 'translate('+left+'px,'+top+'px)';
+		$(canvas).get(0).style.WebkitTransform += ' translate('+left+'px,'+top+'px)';
+		$(canvas).get(0).style.OTransform += 'translate('+left+','+top+')';
+	}
+	
+	function outerScrollHeight(elm, includeMargin) {
+		// When an element's content does not generate a vertical scrollbar, then its scrollHeight property is equal to its clientHeight property.
+        // https://developer.mozilla.org/en/DOM/element.scrollHeight
+		if ($.browser.mozilla || $.browser.opera) {
+			var originalOverflowStyle = $(elm).get(0).style.overflow;
+			$(elm).get(0).style.overflow = "scroll";
+		}
+		var totalHeight = $(elm).get(0).scrollHeight; //includes padding.
+		if ($.browser.mozilla || $.browser.opera) {
+			$(elm).get(0).style.overflow = originalOverflowStyle;
+		}
+		totalHeight += $(elm).outerHeight(includeMargin) - $(elm).innerHeight();
+		return totalHeight;
+	}
+	
+	function outerScrollWidth(elm, includeMargin) {
+		// When an element's content does not generate a horizontal scrollbar, then its scrollWidth property is equal to its clientWidth property.
+        // https://developer.mozilla.org/en/DOM/element.scrollWidth
+		if ($.browser.mozilla || $.browser.opera) {
+			var originalOverflowStyle = $(elm).get(0).style.overflow;
+			$(elm).get(0).style.overflow = "scroll";
+		}
+		var totalWidth = $(elm).get(0).scrollWidth; //includes padding
+		if ($.browser.mozilla || $.browser.opera) {
+			$(elm).get(0).style.overflow = originalOverflowStyle;
+		}
+		totalWidth += $(elm).outerWidth(includeMargin) - $(elm).innerWidth();
+		return totalWidth;
 	}
 	
 	return {
